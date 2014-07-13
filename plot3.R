90643ec1e15f2f42bda7f47d86db09f22e6f76cc
RaymondNelson <raymond.nelson@gmail.com>
2014-07-13 18:46:48 -0400

    fixed plot 3 legend
    plot 3 legend

--- plot3.R
+++ plot3.R
@@ -33,6 +33,9 @@ dateTime <- as.POSIXct(dateTime)
 plot(x = dateTime, y = subDat$Sub_metering_1, type = "l", ylab = "Engergy Sub_metering")
 points(x = dateTime, y = subDat$Sub_metering_2, type = "l", col = "red")
 points(x = dateTime, y = subDat$Sub_metering_3, type = "l", col = "blue")
+legend("topright", legend = c("Sub_metering_1", "Sub_metering_2", "Sub_metering_2"), 
+       lty = c(1, 1, 1), 
+       col = c("black", "red", "blue"))
 
 plotName <- "plot3.png"
 
