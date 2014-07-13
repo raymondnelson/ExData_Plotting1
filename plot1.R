39340697bc38a00b63821ed113ec351522d4a156
RaymondNelson <raymond.nelson@gmail.com>
2014-07-13 13:07:52 -0400

    first commit
    download and load data
    then select the dates

---
+++ plot1.R
@@ -0,0 +1,28 @@
+######## R script to read power consumption data from UCI 
+# and make plot 1 for Coursera exdata-004
+# 7-7-2014
+# Raymond Nelson
+#
+####################
+
+setwd("~/Documents/RAYMOND/Coursera/Exploratory data analysis/project 1")
+
+fileName <- "./household_power_consumption.txt"
+
+if (!file.exists(fileName)) {
+        fileUrl <- "https://archive.ics.uci.edu/ml/machine-learning-databases/00235/household_power_consumption.zip"
+        dFileName <- "./household_power_consumption.zip"
+        download.file(fileUrl, destfile = dFileName)
+        unzip(dFileName)
+}
+
+dat <- read.csv(fileName, sep=";", stringsAsFactors = FALSE)
+
+datDate <- as.POSIXct(strptime(dat$Date, format = "%d/%m/%Y", tz = "GMT"))
+
+febDates <- which(datDate >= strptime("2007-02-01", format = "%Y-%m-%d", tz = "GMT") & datDate <= strptime("2007-02-02", format = "%Y-%m-%d", tz = "GMT"))
+
+subDat <- dat[febDates,]
+
+
+
